ae8f3afc19bf70584c4c4d9cf5cd1314b2d4ef12
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-03-22 11:47:32 +0100

    Remove fast spec helper

--- spec/fast_spec_helper.rb
+++
@@ -1,8 +0,0 @@
-begin
-  # use `bundle install --standalone' to get this...
-  require_relative '../vendor/bundler/setup'
-rescue LoadError
-  # fall back to regular bundler if the developer hasn't bundled standalone
-  require 'bundler'
-  Bundler.setup
-end
