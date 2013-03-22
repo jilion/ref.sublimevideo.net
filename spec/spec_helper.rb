228874b69a80b63bf34f0f27f3395625e45aab17
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-03-22 11:01:51 +0100

    Ready for CircleCi

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,14 +5,15 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.dirname(__FILE__) + "/../config/environment"
 require 'rspec/rails'
 
-RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-  c.run_all_when_everything_filtered = true
-  c.filter_run_including focus: true
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run_including focus: ENV['FOCUS'] != 'false'
+  config.mock_with :rspec
+  config.fail_fast = ENV['FAST_FAIL'] != 'false'
+  config.order = ENV['ORDER'] || 'random'
 
-  c.mock_with :rspec
-
-  c.before(:each) do
+  config.before(:each) do
     DatabaseCleaner.orm = "mongoid"
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean
